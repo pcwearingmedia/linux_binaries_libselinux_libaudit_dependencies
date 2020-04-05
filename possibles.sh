a09dc7c6887e95d7908c22d5f6e15f6edcda9c3b
P-C Markovski <pcwriter@wearingmedia.com>
2020-04-05 14:31:27 +0000

    Added two comments describing the for loops where /usr/bin/.. binaries' ldd output is checked.

--- possibles.sh
+++ possibles.sh
@@ -7,6 +7,8 @@
 arrBinaries=($(find /usr/bin -type f -executable | xargs -I{} basename {}))
 #arrBinaries=($(ls -ltra /usr/bin | awk {'print $NF;'}))
 
+	# Find all binaries in /usr/bin that depend on libselinux.
+
 for i in "${arrBinaries[@]}"
 do
 	ldds=$(ldd /usr/bin/$i)
@@ -14,3 +16,14 @@ do
 		echo "Command: $i"
 	fi
 done
+
+	# Find all binaries in /usr/bin that depend on libaudit.
+
+for i in "${arrBinaries[@]}"
+do
+        ldds=$(ldd /usr/bin/$i)
+        if echo $ldds | grep "libaudit"; then
+                echo "Command: $i"
+        fi
+done
+
