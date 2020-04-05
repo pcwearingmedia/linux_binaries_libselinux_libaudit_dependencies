20228d816f1ddfd0ead524886a084d0897cf2ccf
P-C Markovski <pcwriter@wearingmedia.com>
2020-04-05 14:25:17 +0000

    'possibles.sh' now creates the array with binaries from /usr/bin with the 'find' command. The for loop creates a temp variable with 'ldd' output for each command, and the if-statement then checks if libselinux exists in that output.

--- possibles.sh
+++ possibles.sh
@@ -4,10 +4,13 @@
 # Purpose: Find all binaries in /usr/bin that depend on libselinux and libaudit .so files.
 
 
-arrBinaries=($(ls -ltra /usr/bin | awk {'print $NF;'}))
+arrBinaries=($(find /usr/bin -type f -executable | xargs -I{} basename {}))
+#arrBinaries=($(ls -ltra /usr/bin | awk {'print $NF;'}))
 
 for i in "${arrBinaries[@]}"
 do
-	echo $i
-	ldd "/usr/bin/$i"
+	ldds=$(ldd /usr/bin/$i)
+	if echo $ldds | grep "libselinux"; then
+		echo "Command: $i"
+	fi
 done
