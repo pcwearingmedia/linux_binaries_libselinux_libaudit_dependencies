de02a6e2c9e60e5a6523640fddbd165f0a2dc00a
P-C Markovski <pcwriter@wearingmedia.com>
2020-06-18 18:06:58 +0000

    Refactored 'possibles.sh' to use a function that is called twice with different text strings to grep for (for dependency searching, particular executables)

--- possibles.sh
+++ possibles.sh
@@ -3,26 +3,22 @@
 # Author: P-C Markovski
 # Purpose: Find all binaries in /usr/bin that depend on libselinux and libaudit .so files.
 
-
 arrBinaries=($(find /usr/bin -type f -executable | xargs -I{} basename {}))
+
 
 	# Find all binaries in /usr/bin that depend on libselinux.
 
-for i in "${arrBinaries[@]}"
-do
-	ldds=$(ldd /usr/bin/$i)
-	if echo $ldds | grep "libselinux"; then
-		echo "Command: $i"
-	fi
-done
+function getDependencies()
+{
+	start=`date +%s`
+	for i in "${arrBinaries[@]}"
+		do
+			ldds=$(ldd /usr/bin/$i)
+			if echo $ldds | grep $1; then
+				echo "Command: $i"
+			fi
+	done
+	end=`date +%s`
+}
 
-	# Find all binaries in /usr/bin that depend on libaudit.
-
-for i in "${arrBinaries[@]}"
-do
-        ldds=$(ldd /usr/bin/$i)
-        if echo $ldds | grep "libaudit"; then
-                echo "Command: $i"
-        fi
-done
 
